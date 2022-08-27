b19564bb163d2af56b46f5347c6e3d0dfab4e4c1
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-27 20:04:47 +0545

    Q2 solved

---
+++ 002.js
@@ -0,0 +1,9 @@
+// 2. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstName  = "Ramesh";
+let lastName = "Kunwar"
+let maritalStatus = "Unmarried"
+let country = "Nepal"
+let age = 20;
+
+console.log(`${firstName} ${lastName} ${maritalStatus} ${country} ${age}`);
