3b35c073b0824be60508ad7ba60fcbb7e0244f28
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-27 20:06:31 +0545

    Q3 solved

---
+++ 003.js
@@ -0,0 +1,5 @@
+// 3. Decalre a varibale and assign value to it and change all the string characters to capital letters using toUpperCase() method.
+
+let fruits = "apple";
+
+console.log(fruits.toUpperCase());
