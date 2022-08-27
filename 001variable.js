bcdcebbf9793eda52e5d162150343213f3448320
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-27 20:02:04 +0545

    Q1 solved

---
+++ 001variable.js
@@ -0,0 +1,12 @@
+// 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+let name = "ramesh";
+let booleanValue = true;
+let notDefined = undefined;
+let empty = null;
+
+
+console.log(typeof(name), name);
+console.log(typeof(booleanValue), booleanValue);
+console.log(typeof(notDefined), notDefined );
+console.log(typeof(empty), empty);
