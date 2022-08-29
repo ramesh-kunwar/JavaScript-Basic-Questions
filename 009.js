393a27609550472fa02f8c0a93830087340b10e3
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-29 07:36:28 +0545

    009 solved

---
+++ 009.js
@@ -0,0 +1,6 @@
+// 9. Use trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+let sentence = " Hello world, Hi there ";
+console.log(sentence);
+let trimmedSentence = sentence.trim()
+console.log(trimmedSentence);
