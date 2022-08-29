a0125f32356f25ea18fe93de112f8a2edc1a534c
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-29 07:42:58 +0545

    010 solved

---
+++ 010.js
@@ -0,0 +1,27 @@
+// 10. Boolean value is either true or false.
+//     - Write three JavaScript statement which provide truthy value.
+//     - Write three JavaScript statement which provide falsy value.
+
+//  TRUTHY VALUES
+if (true) {
+  console.log("truthy value 1");
+}
+
+if (1) {
+  console.log("truthy value 2");
+}
+if ([]) {
+  console.log("truthy value 3");
+}
+
+// FALSY VALUES
+
+if (false) {
+  console.log("falsy values");
+}
+if (NaN) {
+  console.log("falsy values");
+}
+if (undefined) {
+  console.log("falsy values");
+}
