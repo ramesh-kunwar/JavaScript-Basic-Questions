8010c0555ec7e19dd88bd74694bedaa9765599b9
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-29 11:03:36 +0545

    incomplete

---
+++ 024main.js.js
@@ -0,0 +1,5 @@
+// 24. Create a separate countries.js file and store the countries array in to this file, 
+// create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+
+import countryList from "./024countries";
+console.log(countryList)
