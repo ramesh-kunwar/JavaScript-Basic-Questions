6f06130f9e93ed217d90a9c7464733ccb9eae47f
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-29 07:23:36 +0545

    006 solved

---
+++ 006.js
@@ -0,0 +1,7 @@
+// 6. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+// split the string at the comma and change it to an array.
+
+const companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+const newCompanies =  companies.split(",")
+
+console.log(newCompanies);
