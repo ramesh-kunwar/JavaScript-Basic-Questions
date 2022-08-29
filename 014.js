67d0f368eb768bdcf0b48b4f1498599def89bb03
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-29 08:48:10 +0545

    q14 solved

---
+++ 014.js
@@ -0,0 +1,5 @@
+// 14. Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+
+const x1 = 2, y1 = 2, x2 = 6, y2 = 10;
+const slope = (y2-y1) / (x2-x1)
+console.log(`Slope of given points = ${slope}`);
