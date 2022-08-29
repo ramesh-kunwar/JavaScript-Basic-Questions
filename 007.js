3005b211a20c42395593b99139e972b7a3d219c6
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-29 07:28:33 +0545

    007 solved

---
+++ 007.js
@@ -0,0 +1,5 @@
+// 7. Use lastIndexOf to determine the position of the last occurrence of a script.
+
+let sentence = "Hello I am the best programmer in the world and world";
+let result = sentence.lastIndexOf("world");
+console.log(result);
