eabbf0f05eff137cfb66b58025e04b23a73c8657
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-29 10:35:00 +0545

    Q23 solved

---
+++ 023.js
@@ -0,0 +1,6 @@
+// 23. Write a program which tells the number of days in a month, now consider leap year.
+
+const days = 365;
+const leapYearDays = days + 1;
+const month = leapYearDays / 30;
+console.log(month);
