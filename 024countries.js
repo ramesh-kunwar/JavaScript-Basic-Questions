616ada1e800422d75b54103637b411ac5754963c
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2022-08-30 10:14:28 +0545

    not complete

--- 024countries.js
+++ 024countries.js
@@ -1,4 +1,4 @@
-export const countryList = [
+module.export  = [
 	"Afghanistan",
 	"Albania",
 	"Algeria",
---
+++ 032.js
@@ -0,0 +1,18 @@
+// 32. Array Questions
+//     - Extract all the countries contain the word 'land' from the countries array and print it as array
+//     - Find the country containing the hightest number of characters in the countries array
+//     - Extract all the countries contain the word 'land' from the countries array and print it as array
+//     - Extract all the countries containing only four characters from the countries array and print it as array
+//     - Extract all the countries containing two or more words from the countries array and print it as array
+//     - Reverse the countries array and capitalize each country and stored it as an array
+
+
+
+//     - Extract all the countries contain the word 'land' from the countries array and print it as array
+const countries = ["Finland", "Poland", "Germany", "Netherland", "China"]
+console.log(countries.includes("land"));
+//     - Find the country containing the hightest number of characters in the countries array
+//     - Extract all the countries contain the word 'land' from the countries array and print it as array
+//     - Extract all the countries containing only four characters from the countries array and print it as array
+//     - Extract all the countries containing two or more words from the countries array and print it as array
+//     - Reverse the countries array and capitalize each country and stored it as an array
